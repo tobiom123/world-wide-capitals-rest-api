94edb57c537c838a646cc4a00f5bab5b21f9075e
Oluwatobi Omotosho <tobiom@Oluwatobis-Mac-Studio.local>
2023-07-26 21:07:48 +0100

    Add routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(['auth:sanctum'])->get('/countries', [\App\Http\Controllers\CountryController::class, 'index']);
+Route::middleware(['auth:sanctum'])->get('/countries-only', [\App\Http\Controllers\CountryController::class, 'getCountriesArray']);
+Route::middleware(['auth:sanctum'])->get('/random-country', [\App\Http\Controllers\CountryController::class, 'getRandomCountry']);
